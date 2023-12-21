d4dca2c0305dc9ad5dcbe7d0b2a94970c553c549
MTLphys <larsonmw@mail.uc.edu>
2023-12-21 16:22:16 -0500

    Documentation please

--- ToolBox/Efield.py
+++ ToolBox/Efield.py
@@ -7,6 +7,22 @@ Functions describing the Efield used to excite the coupled exciton plasmon syste
 import numpy as np 
 
 def Ef(t,DT,E0,omega1,omega2=0,omega3=0,Epulse=True,Dt1=0,Dt2=0): 
+    """An Efield function describing the interaction with the  
+
+    Args:
+        t (array or double): the linspace of the time window for the simulation
+        DT (double): The width of the pulses
+        E0 (double): The strength of the e-field
+        omega1 (double):angular frequency of the first pulse 
+        omega2 (int, optional): angular frequency of second pulse set to omega 1 if zero. Defaults to 0.
+        omega3 (int, optional):  angular frequency of third pulse set to omega 1 if zero. Defaults to 0.
+        Epulse (bool, optional): Pulsed(true) or CW mode(false).Defaults to True
+        Dt1 (double, optional): delay between pulse 1 and 2. Defaults to 0.
+        Dt2 (double, optional): delay between pulse 2 and 3. Defaults to 0.
+
+    Returns:
+        _ndarray_: an exciting pulse series 
+    """
     Efield = np.zeros(len(t))
     omega2 = omega2+(omega2==0)*omega1 
     omega3 = omega3+(omega3==0)*omega1
@@ -48,5 +64,5 @@ def MPulse(t,E0,omega1,Dt=0,Phi=0):
     Efield= E0*np.sin(omega1*t-Dt-Phi)*pulse(t-Dt,5*2*np.pi/omega1)
     return Efield 
 def pulse(t,dt):
-    #pulse envelope 
+    """The pulse shape function for a sinusoidal puls shape which tends to zero at either side""" 
     return np.sin(np.pi*t/dt)**2*np.heaviside(t,.5)*np.heaviside(dt-t,.5)
